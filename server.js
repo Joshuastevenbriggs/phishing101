605a89f35db253c5c5fce830a2052fb54bfc8952
Joshuastevenbriggs <117618675+Joshuastevenbriggs@users.noreply.github.com>
2024-12-12 14:11:30 +0530

    need more work

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const app = express();
 const PORT = 3000;
 const cors = require('cors');
 app.use(cors());
-// Middleware to parse JSON requests
 app.use(bodyParser.json());
 
-// Route to handle form submissions
+
 app.post("/login", (req, res) => {
     console.log("Form Submission:");
     console.log("Email:", req.body.email);
@@ -16,7 +15,7 @@ app.post("/login", (req, res) => {
     res.send("Form data received successfully!");
 });
 
-// Start the server
+
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
