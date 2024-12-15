9352615838a3c493190ee51c8e94172483fa3ed5
shubham2624 <70198257+shubham2624@users.noreply.github.com>
2024-12-15 16:30:14 +0530

    Update prog.py

--- prog.py
+++ prog.py
@@ -2,12 +2,14 @@ def main():
     url1 = "https://www.linkedin.com/checkpoint/lg/sign-in-another-account?midToken=AQHsdMHMoQXuqA"
     url2 = "https://github.com/signup?ref_cta=Sign+up&ref_loc=header+logged+out&ref_page=%2F&source=header-home"
     url3 = "https://www.instagram.com/"
-
+    url4 = "https://www.facebook.com/"
+   
     while True:
         print("\nMenu:")
         print("1. LinkedIn")
         print("2. GitHub")
         print("3. Instagram")
+        print("4. Facebook")
         
         choice = input("Enter your choice (1-3): ")
 
@@ -17,7 +19,8 @@ def main():
             print(f"\nGitHub URL: {url2}")
         elif choice == '3':
             print(f"\nInstagram URL: {url3}")
-            break
+        elif choice == '4':
+            print("\nFacebook URL: {url4}")
         else:
             print("\nInvalid choice.")
 
